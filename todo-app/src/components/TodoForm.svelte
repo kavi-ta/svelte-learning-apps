c82a993273a4f332522dbf1dc7d29d3b6e720756
kavi-ta <kavisharma2807@gmail.com>
2023-09-14 11:28:28 +0530

    Todo-App using svelte

---
+++ todo-app/src/components/TodoForm.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {addTodo} from "../stores/todoStore"
+
+    let todo = "";
+    const handleSubmit = ()=>{
+        addTodo(todo);
+        todo = "";
+    }
+</script>
+
+<form action="" class="my-6" on:submit|preventDefault = {handleSubmit}>
+    <div class="flex flex-col text-sm mb-2">
+        <label for="todo" class="font-bold mb-2 text-gray-800">Todo</label>
+        <input bind:value={todo} class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" type="text" name="todo" placeholder="Watcha gotta do?">
+    </div>
+    <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
+</form>
---
+++ todo-app/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content:[
+    './src/**/*.{svelte, html, js, ts}'
+  ],
+  darkMode: false,
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
